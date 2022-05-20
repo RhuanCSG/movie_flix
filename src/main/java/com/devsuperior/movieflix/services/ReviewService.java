cba78888651fc6e1de4e13ea20994b26c82b8408
Rhuan Carlos <rhuancsg@gmail.com>
2022-05-20 16:52:20 -0300

    Entities, Exceptions and Validations created

---
+++ src/main/java/com/devsuperior/movieflix/services/ReviewService.java
@@ -0,0 +1,32 @@
+package com.devsuperior.movieflix.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.ReviewRepository;
+
+@Service
+public class ReviewService {
+
+	@Autowired
+	private ReviewRepository repository;
+	
+	@Autowired
+	private AuthService authService;
+	
+	@Transactional
+	public ReviewDTO insert(ReviewDTO dto) {
+		authService.validadeMember();
+		Review entity = new Review();
+		entity.setText(dto.getText());
+		entity.setUser(new User(dto.getUserId(),null, null,null));
+		entity.setMovie(new Movie(dto.getMovieId(),null,null,null,null,null,null));
+		entity = repository.save(entity);
+		return new ReviewDTO(entity);
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Bob Burn', 'bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Ana Green', 'ana@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_VISITOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_MEMBER');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
+INSERT INTO tb_movie (title, sub_title, year, img_url, synopsis) VALUES ('Homem Aranha', 'Sem volta para casa', 2022, 'https://tm.ibxk.com.br/2021/11/16/16082712372025.jpg?ims=1200x675', 'xxx');
